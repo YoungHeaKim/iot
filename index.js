b2b2e40e90cad4c0d8ce37834db47ffe2d5d91a4
KimYH <dydgo9507@gmail.com>
2019-11-11 18:22:27 +0900

    color 팔렛트 사용 및 백엔드에 값 가져오는 지 확인완료

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ import serialPort from 'serialport';
 // const portName = '/dev/cu.usbmodem14101';
 
 const com6 = new serialPort('/dev/cu.usbmodem144301', {
-  baudRate: 9600,
-  // defaults for Arduino serial communication
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  flowControl: false,
+    baudRate: 9600,
+    // defaults for Arduino serial communication
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
 });
 
 com6.on('open', function () {
-  console.log('open serial communication');
-  // 아두이노 데이터 가져오는 부분
-  // com6.on('data', function(data) { console.log('Value: ', data[0]);});
+    console.log('open serial communication');
+    // 아두이노 데이터 가져오는 부분
+    // com6.on('data', function(data) { console.log('Value: ', data[0]);});
 });
 
 // view engine setup
@@ -35,13 +35,25 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.status(200).render('view.ejs');
+    res.status(200).render('view.ejs');
+    // res.status(200).render('test.ejs');
 });
 
-app.get('/controller/:id', function (req, res) {
-  console.log(req.params.id);
-  com6.write(req.params.id);
-  res.status(200).send('LED Controll OK!!');
+app.get('/controller/:_id', function (req, res) {
+    const Str = '#';
+    const hex = Str.concat(req.params._id);
+    function hexToRgb(hex) {
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+    let RGB = hexToRgb(hex);
+    console.log(RGB);
+    // com6.write(req.params.id);
+    res.status(200).send('LED Controll OK!!');
 });
 
 app.listen(port, () => console.log('Example app listening on port', port));
