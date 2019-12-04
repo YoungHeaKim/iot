ff32d479c938215e21e2995aa6a41e8811540012
KimYH <dydgo9507@gmail.com>
2019-12-04 22:50:47 +0900

    led 제어 및 온도센서 값 가져오고 온도에 따른 변화 색 변화 완료

--- index1.js
+++ index1.js
@@ -7,14 +7,13 @@ const app = express();
 let port = 3000;
 
 import serialPort from 'serialport';
-import Readline from '@serialport/parser-readline';
 
 // 시리얼 포트 설정
 // COM6 : 아두이노가 연결된 포트
 // 아래 ####은 본인 아두이노의 시리얼 포트에 맞게 경로 입력하기
 // const portName = '/dev/cu.usbmodem145301';
 
-const com6 = new serialPort('/dev/cu.usbmodem144301', {
+const com6 = new serialPort('/dev/cu.usbmodem145301', {
     baudRate: 9600,
     // defaults for Arduino serial communication
     dataBits: 8,
@@ -22,13 +21,15 @@ const com6 = new serialPort('/dev/cu.usbmodem144301', {
     stopBits: 1,
     flowControl: false,
 });
+const Readline = require('@serialport/parser-readline');
+const parser = port.pipe(new Readline({delimiter: '\n'}));
 
 com6.on('open', function () {
     console.log('open serial communication');
-    com6.flush(() => {
-        const parser = com6.pipe(new Readline({delimiter: '\r'}));
-        return parser.on('data', line => console.log(line));
-    });
+    // 아두이노 데이터 가져오는 부분
+    // com6.on('data', function (data) {
+    //     console.log('Value: ', data);
+    // });
 });
 
 // view engine setup
@@ -43,18 +44,21 @@ app.get('/', function (req, res) {
     // res.status(200).render('test.ejs');
 });
 
-// TODO: 이부분에서 data값 보내주는 것만 하면 완성
 // led제어 해주는 부분
-app.get('/controller/:_id', function (req, res) {
-    console.log(new Date());
-    const {_id} = req.params;
-    if (_id === "temp") {
-        com6.write(_id);
-    } else {
-        com6.write("color/" + _id);
-    }
+aapp.get('/controller/:_id', function (req, res) {
+    console.log('req.params._id', req.params._id);
+    // TODO: 값을 두번 보내는 오류
+    let hexFront = '0x';
+    let hex = hexFront.concat(req.params._id);
+    console.log('전', hex);
+    com6.write(hex);
+    console.log('후');
     res.status(200).send('LED Controll OK!!');
+});
 
+// temp 온도 보내주는 부분
+app.get('/temp', function (req, res) {
+    res.status(200).send('동권쓰');
 });
 
 app.listen(port, () => console.log('Example app listening on port', port));
--- iot_1/iot_1.ino
+++ iot_1/iot_1.ino
@@ -1,36 +1,70 @@
-#define LED_01 11 // LED_Green
-#define LED_02 12 // LED_Red
-#define LED_03 13 // LED_Blue
+#include <Servo.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LED_01 13 // LED_Red
+#define LED_02 12 // LED_Green
+#define LED_03 11 // LED_Blue
 
 #define LED_NUM 3
-unsigned int ledArray[] = { LED_01,LED_02, LED_03 } ;
-
+unsigned int ledArray[] = { LED_01, LED_02, LED_03 } ;
+// temp에 관한 변
+const int TEMP = A0;
 
 void setup() {
   Serial.begin(9600);
 
-  for(int i = 0 ; i < LED_NUM ; i++) {
+  for (int i = 0 ; i < LED_NUM ; i++) {
     pinMode(ledArray[i], OUTPUT) ;
   }
 }
 
 void loop() {
-  if(Serial.available()) {
-      char c = Serial.read();
-//      Serial.write(c);
-      if(c == 'A') {
-            for(int i = 0 ; i < LED_NUM ; i++) {
-                digitalWrite(ledArray[i], HIGH) ;
-            }
-           return;
-      }
-      if(c == 'X') {
-            for(int i = 0 ; i < LED_NUM ; i++) {
-                digitalWrite(ledArray[i], LOW) ;
-            }
-           return;
-      }
-      int ledNum = c - '1';
-      digitalWrite(ledArray[ledNum],!digitalRead(ledArray[ledNum])) ;
+  String type;
+  long color = 0;
+  if (Serial.available()) {
+    String getString = Serial.readString();
+
+    int index1 = getString.indexOf('/');
+    int index2 = getString.length();
+    type = getString.substring(0, index1);
+    String colorHex = getString.substring(index1 + 1, index2);  //FF0000
+    String frontHex = "0x";
+    colorHex = frontHex + colorHex;   // 문자 합친거: 0xff0000
+
+    // colorHex.c_str() -> colorHex 문자배열로 변환
+    color = (long)strtol(colorHex.c_str(), NULL, 16);
   }
+  // type 나누는 부분
+  if (type == "color") {
+    setColor(color);
+  } else if (type == "temp") {
+    int val = analogRead(TEMP);
+
+    float valu = val * 5000.0 / 1024.0;
+    float TempDotC = (valu - 500) / 10.0;
+
+    Serial.print(TempDotC);
+    Serial.println("ºC");
+
+    if (TempDotC < 20) {
+      setColor(0x0000FF);
+    } else if (20 < TempDotC && TempDotC < 22) {
+      setColor(0x008080);
+    } else if (22 < TempDotC && TempDotC < 24) {
+      setColor(0x00FF00);
+    }  else if (24 <= TempDotC && TempDotC < 26) {
+      setColor(0xFFFF00);
+    } else if (26 <= TempDotC && TempDotC < 28) {
+      setColor(0xFFA500);
+    } else {
+      setColor(0xFF0000);
+    }
+  }
+}
+
+void setColor(long color) {
+  analogWrite(ledArray[0], color >> 16);
+  analogWrite(ledArray[1], color >> 8 & 0xFF);
+  analogWrite(ledArray[2], color & 0xFF);
 }
